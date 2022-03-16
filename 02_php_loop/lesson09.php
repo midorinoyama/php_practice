1cc70f08f69e1e790063f089545c2fbdd77a8cb6
midorinoyama <fujiaqa@gmail.com>
2022-03-16 22:33:36 +0900

    [Finish]02_loop

--- 02_php_loop/lesson09.php
+++ 02_php_loop/lesson09.php
@@ -18,6 +18,31 @@ $arr = [
     'r2' => ['c1' => 7, 'c2' => 8, 'c3' => 12],
     'r3' => ['c1' => 25, 'c2' => 9, 'c3' => 130]
 ];
+
+//行ごとの合計(array_sum関数で配列の要素の合計を取得)
+$row_total = [
+    'r1' => array_sum($arr['r1']),
+    'r2' => array_sum($arr['r2']),
+    'r3' => array_sum($arr['r3'])
+];
+//print_r($row_total);
+//echo array_sum($arr['r1']);
+
+//列ごとの合計(array_column関数で指定したキーのみを取得)
+$column_total = [
+  'c1' => array_sum(array_column($arr, 'c1')),
+  'c2' => array_sum(array_column($arr, 'c2')),
+  'c3' => array_sum(array_column($arr, 'c3'))
+];
+//print_r($column_total);
+
+//総合計
+$all_total = 0;
+foreach ($column_total as $total) {
+    $all_total += $total;
+}
+//echo $all_total;
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -36,5 +61,33 @@ th, td {
 </head>
 <body>
     <!-- ここにテーブル表示 -->
+    <table border="1">
+        <tr><td></td><td>c1</td><td>c2</td><td>c3</td><td>横合計</td></tr>
+        <?php
+        //縦r1~r3を表示
+        for ($i = 1; $i <= count($arr); $i++) {
+            //キーを指定して値を取得
+            $row_key = "r{$i}";
+            echo "<tr><td>{$row_key}</td>";
+            //r1~r3各行の要素を表示
+            for ($j = 1; $j <= count($arr[$row_key]); $j++) {
+                //キーを指定して値を取得
+                $column_key = "c{$j}";
+                echo "<td>{$arr[$row_key][$column_key]}</td>";
+            }
+            //行の合計を表示
+            echo "<td>{$row_total[$row_key]}</td></tr>";
+        }
+        echo "<tr><td>縦合計</td>";
+        //列の合計を表示
+        for ($j = 1; $j <= count($column_total); $j++) {
+            $column_key = "c{$j}";
+            echo "<td>{$column_total[$column_key]}</td>";
+        }
+        //総合計を表示
+        echo "<td>{$all_total}</td></tr>"
+        ?>
+    </table>
+
 </body>
 </html>
--- 02_php_loop/lesson10.php
+++ 02_php_loop/lesson10.php
@@ -21,6 +21,27 @@ $arr = [99, 3, 12, 45, 60, 100, 31, 7, 28];
 $arr = [99, 3, 12, 45, 60, 100, 31, 7, 28];
 
 // ここで並び替え処理
+//並び替える前
+/*for ($i = 0; $i < count($arr); $i++) {
+    echo $arr[$i]."  ";
+}
+echo "\n";*/
+for ($i = 0; $i < count($arr) - 1; $i++) {
+    for ($j = 0; $j < count($arr) - 1; $j++) {
+        if ($arr[$j] > $arr[$j+1]) {
+            $tmp = $arr[$j];
+            $arr[$j] = $arr[$j + 1];
+            $arr[$j + 1] = $tmp;
+
+            //１つずつ入れ替わる途中経過
+            /*for ($k = 0; $k < count($arr); $k++) {
+                echo $arr[$k]."  ";
+            }
+            echo "\n";*/
+        }
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -30,5 +51,16 @@ $arr = [99, 3, 12, 45, 60, 100, 31, 7, 28];
 </head>
 <body>
     <!-- ここに並び替え後を表示 -->
+    <?php
+    $bool = true;
+    for ($i = 0; $i < count($arr); $i++) {
+        if ($bool) {
+            echo $arr[$i];
+            $bool = false;
+        } else {
+            echo ", ".$arr[$i];
+        }
+    }
+    ?>
 </body>
 </html>
